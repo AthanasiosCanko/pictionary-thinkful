356b3cda883b6bc0776499c1fc9cb870e4977867
Athanasios Canko <cankoathanasios@gmail.com>
2016-08-23 13:58:15 +0200

    Committing new features...

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ var socket_io = require('socket.io');
 var app = express();
 app.use(express.static('public'));
 
+// Wrapping our Express app in a bidirectional connection
 var server = http.Server(app);
 var io = socket_io(server);
 
-var canvasKey = true, counter = 0, WORDS = [
+// Boolean value that regulates drawer/guesser relationship
+var canvasKey = true;
+
+// Keeps count of online users
+var counter = 0;
+
+// Random word array
+var WORDS = [
     "word", "letter", "number", "person", "pen", "class", "people",
     "sound", "water", "side", "place", "man", "men", "woman", "women", "boy",
     "girl", "year", "day", "week", "month", "name", "sentence", "line", "air",
@@ -24,33 +32,44 @@ var canvasKey = true, counter = 0, WORDS = [
     "space"
 ];
 
+// Whenever we detect a connection
 io.on("connection", function(socket) {
 	console.log("New canvas...");
 	console.log(canvasKey);
 	
+	// Random number to choose from the array
 	var number =  parseInt(Math.random() * (WORDS.length - 1));
 	console.log(number);
 	
 	counter += 1;
 	
+	// Emitting random array element
 	socket.on("randomWord", function() {	
 		socket.emit("randomWord", WORDS[number]);
 	});
 	
+	// Emiting canvas key
 	io.emit("key", canvasKey);
+	// Emitting counter value
+	io.emit("counter", counter);
 	canvasKey = false;
 	
-	
+	// Emitting drawing to all users
 	socket.on("draw", function(position) {
 		io.emit("draw", position);
 	});
 	
+	// Emitting guess text to all users
 	socket.on("guess", function(guessText) {
 		io.emit("guess", guessText);
 	});
 	
+	// Whenever a user disconnects, the following happens
 	socket.on("disconnect", function() {
+
 		counter -= 1;
+			
+		io.emit("counter", counter);
 		if (counter === 0) {
 			canvasKey = true;
 		}
--- public/main.js
+++ public/main.js
@@ -2,18 +2,28 @@ $(document).ready(function() {
 	var socket = io();
 	var canvas, context;
 	
+	// Setting up canvas specs
   	canvas = $('canvas');
 	context = canvas[0].getContext('2d');
 	canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
 
+	// We detect is a user is a drawer or guesser
 	socket.on("key", function(key) {
 		if (key === true) {
+			var drawing = false;
+			
 			socket.emit("randomWord");
 		    $("#guess").remove();
 		    
-			var drawing = false;
+		    /* <<< BETA >>>
+		    
+		    $(".btn-warning").on("click", function() {
+		    	console.log(this.id);
+		    });
+		    */
     
+    		// Mouse movement detection
 		    canvas.on('mousemove', function(event) {
     
     			if (drawing === true) {
@@ -23,29 +33,47 @@ $(document).ready(function() {
     			}
     			
     		});
-    
-    		canvas.on('mousedown', function(event) {
-    			drawing = true;
-    		});
-    
-    		canvas.on('mouseup', function(event) {
-    			drawing = false;
-    		});  
     		
+    		// Function that activates mousedown actions
+    		function mouseDown(bool) {
+    			canvas.on('mousedown', function(event) {
+    				drawing = bool;
+    			});
+    		}
+		
+			// Getting a random word from the server and displaying it to the drawer
     		socket.on("randomWord", function(word) {
-    			console.log("word");
     			$("#word").html("Word to draw: " + word);
     		});	
+    		
+    		// Keeping track of online users for the drawer
+    		socket.on("counter", function(counter) {
+    			if (counter === 1) {
+    				$("#drawOrNot").html("<p style='color: crimson'>You cannot draw. No guesser online yet.</p>");
+    				// Deactivates mouse action
+    				mouseDown(false);
+    			}
+    			else {
+    				$("#drawOrNot").html("<p style='color: green'>Guesser popped up. Now you can draw!</p>");
+    				// Activates mouse action
+    				mouseDown(true);
+    
+		    		canvas.on('mouseup', function(event) {
+    					drawing = false;
+    				});  
+    			}
+    		});
 		}
 		else {
-			
+			// Guesser code
 			var guessBox = $('#guess input');
-    		
+    	
     		var onKeyDown = function(event) {
    				if (event.keyCode != 13) {
         			return;
     			}
-
+				
+				// When we hit Enter, we send the guess to the server
 			    socket.emit("guess", guessBox.val());
     			guessBox.val('');
 			};
@@ -54,15 +82,16 @@ $(document).ready(function() {
 		}
 	});
 	
+	// When we receive a drawing from the server, we display it to our page
 	socket.on("draw", function(position) {
-				console.log("Hello!");
 		        context.beginPath();
         		context.arc(position.x, position.y,
                         6, 0, 2 * Math.PI);
     		    context.fill();
     		});  
-    		
+    	
+    // When we receive a guess, we add it as a button for the drawer to click if correct
     socket.on("guess", function(guessText) {
-   	 	$("#text").append("<p>" + guessText + "</p>");
+   	 	$("#text").append("<button id='" + guessText + "' class='btn btn-warning' style='width: 150px'>" + guessText + "</button><br>");
     });
 });
